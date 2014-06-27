ac9e4e3857f57fa99d49ff292fce9cfe9b1f9d78
Tomer Elmalem <telmalem@gmail.com>
2014-06-27 13:17:58 -0700

    Add example rails application

---
+++ v2/ruby/rails/config/initializers/yelp.rb
@@ -0,0 +1,6 @@
+Yelp.client.configure do |config|
+  config.consumer_key = ENV['YELP_CONSUMER_KEY']
+  config.consumer_secret = ENV['YELP_CONSUMER_SECRET']
+  config.token = ENV['YELP_TOKEN']
+  config.token_secret = ENV['YELP_TOKEN_SECRET']
+end
