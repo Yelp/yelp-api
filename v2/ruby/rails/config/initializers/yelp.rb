190de787aba1658bf6e51e98f7255e950cbab519
Tomer Elmalem <telmalem@gmail.com>
2015-10-14 18:42:45 -0700

    Remove the rails example, that lives in at http://github.com/yelp/yelp-rails-example

--- v2/ruby/rails/config/initializers/yelp.rb
+++
@@ -1,6 +0,0 @@
-Yelp.client.configure do |config|
-  config.consumer_key = ENV['YELP_CONSUMER_KEY']
-  config.consumer_secret = ENV['YELP_CONSUMER_SECRET']
-  config.token = ENV['YELP_TOKEN']
-  config.token_secret = ENV['YELP_TOKEN_SECRET']
-end
