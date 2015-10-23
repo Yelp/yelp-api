709caae491c456fd51ed6859c0e760795e7f773f
Ken Mitton <mittonk@gmail.com>
2015-10-23 11:39:12 -0400

    Windows batch for Java.  Closes #62.

---
+++ v2/java/run.bat
@@ -0,0 +1,5 @@
+set EXAMPLE_CP=lib\scribe-1.3.5.jar;lib\json_simple-1.1.jar;lib\jcommander-1.35.jar;.
+
+if not exist build mkdir build
+javac -classpath %EXAMPLE_CP% -d build YelpAPI.java
+java -classpath %EXAMPLE_CP%;build YelpAPI %*
