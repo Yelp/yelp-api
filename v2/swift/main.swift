ea805e3ca8878c00df24b9ae94ef1a0f24a4a1c7
Steven Sheldon <steven@sasheldon.com>
2016-04-23 18:06:09 -0700

    Empty Swift project.

---
+++ v2/swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  YelpAPISample
+//
+//  Created by Steven Sheldon on 4/23/16.
+//  Copyright © 2016 Yelp. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
